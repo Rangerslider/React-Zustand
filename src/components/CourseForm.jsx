499d5984f1c7c24aebba0a9ec2d9ea59e80db04d
nuri <nuri1999sept@gmail.com>
2023-07-27 21:39:17 +0600

    zustand 1.0.0

---
+++ src/components/CourseForm.jsx
@@ -0,0 +1,43 @@
+//here we show how to add data or pooling data and store to ta local storage
+
+import React, {useState} from 'react'
+import useCourseStore from '../app/courseStore'
+
+const CourseForm = () => {
+
+    const addCourse = useCourseStore((state) => state.addCourse)
+
+    const [courseTitle, setCourseTitle] = useState("")
+    console.log("CourseForm Rendererd");
+
+    const handleCourseSubmit = () => {
+        if (!courseTitle) return alert("please add a course title");
+        addCourse({
+            id: Math.ceil(Math.random() * 1000000),
+            title: courseTitle
+        })
+        setCourseTitle("")
+
+    }
+
+
+  return ( 
+    <div className="form-container">
+        <input 
+        value={courseTitle}
+        onChange={(e) => {
+            setCourseTitle(e.target.value) //e=event
+        }}
+        className="form-input" />
+        <button 
+        onClick={() => {
+            handleCourseSubmit();
+        }}
+        className="form-submit-btn">
+            Add Course
+        </button>
+    </div>
+  )
+}
+
+export default CourseForm
---
+++ src/components/CourseList.jsx
@@ -0,0 +1,50 @@
+//here we show how to pool information to the store and how we manpulated the information and storage the  updated
+import React from 'react'
+import useCourseStore from '../app/courseStore'
+
+
+const CourseList = () => {
+    const {courses, removeCourse, toggleCourseStatus} = useCourseStore(
+        (state) => ({
+            courses: state.courses,
+            removeCourse: state.removeCourse,
+            toggleCourseStatus: state.toggleCourseStatus
+        })
+    )
+  return (
+    <>
+    <ul>
+        {courses.map((course, i) => {
+            return (
+                <React.Fragment key={i}>
+                    <li
+                    className={`course-item`}
+                    style={{
+                        backgroudColor: course.completed ? "#00FF0044" : "white"
+                    }}
+                    >
+                        <span className="course-item-col-1">
+                            <input 
+                            checked={course.completed}
+                            type="checkbox"
+                            onChange={(e) => {
+                                toggleCourseStatus(course.id)
+                            }}
+                            />
+                        </span>
+                        <span>{course?.title}</span>
+                        <button 
+                        onClick={() => {
+                            removeCourse(course.id)
+                        }}
+                        className="delete-btn">Delete</button>
+                    </li>
+                </React.Fragment>
+            )
+        })}
+    </ul>
+    </>
+  )
+}
+
+export default CourseList
